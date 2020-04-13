5e801181533dc6cfb3f47ad34dee2c886f213725
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-04-13 22:53:31 +0900

    掲示板課題の作成開始

--- keiziban.php
+++ keiziban.php
@@ -1,5 +1,9 @@
 <?php
 
+$name = $_POST['name'];
+$user = $_POST['user'];
+$content = $_POST['contet'];
+
 
 ?>
 
@@ -9,8 +13,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/base.css">
-  <link rel="stylesheet" href="/css/index.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/index.css">
   <title>PHP｜課題掲示板</title>
 </head>
 <body>
@@ -25,18 +29,18 @@
   </div>
 
   <div class="post-form">
-    <form>
+    <form action="" method="POST">
       <div class="form-group">
-        <label for="title">投稿タイトル</label>
-        <input type="text" class="form-control-sm" id="title">
+        <p><label for="title">投稿タイトル</label></p>
+        <p><input type="text" class="form-control-sm" id="title" name="title"></p>
       </div>
       <div class="form-group">
-        <label for="user">投稿者</label>
-        <input type="text" class="form-control-sm" id="user">
+        <p><label for="user">投稿者</label></p>
+        <p><input type="text" class="form-control-sm" id="user" name="user"></p>
       </div>
       <div class="form-group">
-        <label for="title">投稿内容</label>
-        <textarea class="form-control-lg" id="content"></textarea>
+        <p><label for="title">投稿内容</label></p>
+        <p><textarea class="form-control-lg" id="content" name="content"></textarea></p>
       </div>
 
       <input type="submit" class="btn btn-primary" value="投稿">
